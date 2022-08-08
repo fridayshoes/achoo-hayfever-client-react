da2f19778c65b9fdf756e3495a74a4a52b450f56
fridayshoes <one_amp@hotmail.com>
2022-08-08 14:25:20 +0100

    React form accepts email

--- src/components/User.js
+++ src/components/User.js
@@ -3,39 +3,47 @@ import React, { Component } from 'react';
 class User extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {  
-				email: ""
-			};
+		this.state = { value: '' };
+		
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		console.log(this.handleSubmit);
 		}
 
-		handleChange = (event) => {
-			this.setState({[event.target.name]: event.target.value});
-  	}
+	handleChange(event) {
+		console.log("form input change")
+		console.log(event)
+		// this.setState({[event.target.email]: event.target.email});
+		this.setState({value: event.target.value});
+		console.log(event.target.email)
+	}
 
-		handleSubmit = (event) => {
-			alert('A form was submitted: ' + this.state);
+	handleSubmit(event) {
+		console.log("form submit")
+		console.log(event)
+		alert('A form was submitted: ' + this.state.value);
 
-			fetch('http://localhost:3000/store-data', {
-        method: 'POST',
-        // We convert the React state to JSON and send it as the POST body
-        body: JSON.stringify(this.state)
-			}).then(function(response) {
-        console.log("hello we're here")
-				console.log(response)
-				return response.json();
-      });
+		fetch('http://localhost:3000/users', {
+			method: 'POST',
+			// We convert the React state to JSON and send it as the POST body
+			body: JSON.stringify(this.state)
+		}).then(function(response) {
+			console.log("fetch response")
+			console.log(response)
+			return response.json();
+		});
 
-			event.preventDefault();
-}
+		event.preventDefault();
+	}
 
 	render() {
 		return (
-			<div><h2 class="title">Sign up</h2>
+			<div><h2 className="title">Sign up</h2>
 			<form onSubmit={this.handleSubmit}>
-				<label for="email">Email:
-				<input type="text" id="email" email="email" value={this.state.value} onChange={this.handleChange} />
+				<label for="email">Email: 
+				<input type="text" id="email" value={this.state.value} onChange={this.handleChange} />
 				</label>
-				<input class="post-button" id="submit" type="submit" value="Submit" />
+				<input type="submit" value="Submit" />
 			</form> 
 			</div>
 		);
