a98d3bece2c8c35828eab720ef78d9dd906ecd4a
Daniel <mbatadaniel@gmail.com>
2022-08-09 11:25:59 +0100

    refactored to change location component and also pollen component

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Location extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: ''};
+    this.state = {
+      value: "",
+      pollenResponse: [],
+      error: null,
+      isLoaded: false,
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    };
-  
-  handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setLocation = props.setLocation.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
 
   handleSubmit(event) {
-    alert('The location you entered was:' + this.state.value)
+    alert("The location you entered was:" + this.state.value);
     event.preventDefault();
-
-  fetch('http://localhost:3000/pollen', {
-  method: "POST",
-  headers: {
-    'Content-type': 'application/json'
-  },
-  body: JSON.stringify(this.state)
-})
-.then((response) => response.json())
-.then((result) => {
-  console.log("location result")
-  console.log(result)
-})
+    this.setLocation(this.state.value);
   }
 
-  render(){
+  render() {
+    // const { error, isLoaded, pollenResponse } = this.state;
     return (
       <div>
+        {/* {pollenResponse.updatedAt} */}
         <form onSubmit={this.handleSubmit}>
-      <label>Enter your location: 
-        <input 
-          type="text" 
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-    </div>
-  )
+          <label>
+            Enter your location:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
--- src/components/Pollen.js
+++ src/components/Pollen.js
@@ -1,83 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Pollen extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       error: null,
-      isLoaded: false,  
-      pollenResponse: [] 
+      isLoaded: false,
+      pollenResponse: [],
     };
+    this.location = props.location;
   }
 
   callSERVER() {
     fetch("http://localhost:3000/pollen", {
       method: "POST",
       headers: {
-        'Content-type': 'application/json'
+        "Content-type": "application/json",
       },
-      body: JSON.stringify({ location: "London"})
+      body: JSON.stringify(this.location),
     })
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
-          this.setState({ 
+          this.setState({
             isLoaded: true,
-            pollenResponse: result
+            pollenResponse: result,
           });
         },
         // error handler
         (error) => {
           this.setState({
             isLoaded: true,
-            error
+            error,
           });
         }
-      )
+      );
   }
-  
+
   componentDidMount() {
     this.callSERVER();
-  }  
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   this.callSERVER();
+  //   if (prevState.location !== this.state.location) {
+  //     this.callSERVER();
+  //   }
+  // }
 
   render() {
-    
     const { error, isLoaded, pollenResponse } = this.state;
 
     if (error) {
-      return (
-        <div>
-          Error: {error.message}
-        </div>
-      );
+      return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      return (
-        <div>
-          Loading...
-        </div>
-      );
+      return <div>Loading...</div>;
     } else {
       return (
         <div>
           <h1>Achoo! Your pollen monitor</h1>
           {console.log(pollenResponse)}
-          {pollenResponse.map((pollen, index) => 
-          <div key={index}>
-            Grass Pollen: 
-            {pollen.Count.grass_pollen},
-            {pollen.Risk.grass_pollen}<br></br>
-            Tree Pollen:
-            {pollen.Count.tree_pollen},
-            {pollen.Risk.tree_pollen}<br></br>
-            Weed Pollen:
-            {pollen.Count.weed_pollen},
-            {pollen.Risk.weed_pollen}<br></br>
-          </div>)}
+          {pollenResponse.map((pollen, index) => (
+            <div key={index}>
+              Grass Pollen:
+              {pollen.Count.grass_pollen},{pollen.Risk.grass_pollen}
+              <br></br>
+              Tree Pollen:
+              {pollen.Count.tree_pollen},{pollen.Risk.tree_pollen}
+              <br></br>
+              Weed Pollen:
+              {pollen.Count.weed_pollen},{pollen.Risk.weed_pollen}
+              <br></br>
+            </div>
+          ))}
         </div>
       );
-    }  
+    }
   }
-
 }
 
 export default Pollen;
